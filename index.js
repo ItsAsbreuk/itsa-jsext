65d673fcdda32c84b347677175db04272371c8b5
Marco Asbreuk <marco@itsasbreuk.nl>
2016-02-11 13:54:38 +0100

    initial release

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+require("./lib/function");
+require("./lib/object");
+require("./lib/string");
+require("./lib/array");
+require("./lib/json");
+require("./lib/promise");
+require("./lib/math");
